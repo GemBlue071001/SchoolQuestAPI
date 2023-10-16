bf3cbe3b2b5b09a09a7ac7a0c0dcce36fb64b345
nmq <quanquan2411@gmail.com>
2023-10-16 23:21:30 +0700

    update department data and add major data

--- ApplicationContext/Configurations/DepartmentConfiguration.cs
+++ ApplicationContext/Configurations/DepartmentConfiguration.cs
@@ -26,116 +26,20 @@ namespace ApplicationContext.Configurations
                 },
                 new Department()
                 {
-                    Id = Guid.Parse("98b5de7d-6326-47ab-972e-9138c299f10e"),
-                    Code = "SE",
-                    Name = "Cong Nghe Thong Tin",
-                    Description = "lam ve may tinh",
+                    Id = Guid.Parse("b8034210-23fd-4af9-9d7f-f260a0cdef97"),
+                    Code = "KT",
+                    Name = "Kinh tế",
+                    Description = "các ngành về kinh tế",
                     RequiredScore = 22
                 },
                 new Department()
                 {
-                    Id = Guid.Parse("5f68a389-ef79-4a8d-aa8e-1f6e944eb241"),
-                    Code = "UI",
-                    Name = "Do Hoa",
-                    Description = "thiet ke giao dien web, mobile, game, content",
-                    RequiredScore = 20
-                },
-                new Department()
-                {
-                    Id = Guid.Parse("b120b5a0-5eac-4836-96ef-90b9834ea88a"),
-                    Code = "GM",
-                    Name = "Mobile Game",
-                    Description = "lam ve game tren dien thoai",
-                    RequiredScore = 23
-                },
-                new Department ()
-                {
-                    Id = Guid.Parse("7c220b5a-4eac-4536-92ef-90b983fea88b"),
-                    Code = "CS",
-                    Name = "Computer Science",
-                    Description = "Khoa học máy tính",
-                    RequiredScore = 24
-                },
-                new Department()
-                {
-                    Id = Guid.Parse("9e520b5a-2eac-4336-90ef-90b985fe322d"),
-                    Code = "AI",
-                    Name = "Artificial Intelligence",
-                    Description = "Trí tuệ nhân tạo",
-                    RequiredScore = 26
-                },
-                new Department() 
-                {
-                    Id = Guid.Parse("ac120b5a-8fac-4536-95ef-90b983aea09e"),
-                    Code = "IB",
-                    Name = "International Business",
-                    Description = "Kinh doanh quốc tế",
-                    RequiredScore = 22
-                },
-                new Department()
-                {
-                    Id = Guid.Parse("ce320b5a-4eac-4436-92ef-90b985aec10f"),
-                    Code = "MK",
-                    Name = "Marketing",
-                    Description = "Marketing",
-                    RequiredScore = 23
-                },
-                new Department()
-                {
-                    Id = Guid.Parse("df520b5a-3fac-4836-91ef-90b986ced11f"),
-                    Code = "BA",
-                    Name = "Business Administration",
-                    Description = "Quản trị kinh doanh",
-                    RequiredScore = 21
-                },
-                new Department() 
-                {
-                    Id = Guid.Parse("eg620b5a-2ebc-4336-90ef-90b987bed22f"),
-                    Code = "AC",
-                    Name = "Accounting",
-                    Description = "Kế toán",
-                    RequiredScore = 20
-                },
-                new Department()
-                {
-                    Id = Guid.Parse("1f120b5a-2fac-4536-95ef-90b983aec13g"),
-                    Code = "HTM",
-                    Name = "Hospitality & Tourism Management",
-                    Description = "Quản lý khách sạn và du lịch",
-                    RequiredScore = 22
-                },
-                new Department()
-                {
-                    Id = Guid.Parse("1f120b5a-2fac-4536-95ef-90b983aec13g"),
-                    Code = "HTM",
-                    Name = "Hospitality & Tourism Management",
-                    Description = "Quản lý khách sạn và du lịch",
-                    RequiredScore = 22
-                },
-                new Department()
-                {
-                    Id = Guid.Parse("8m920b5a-2dac-4436-92ef-90b985bed1ah"),
-                    Code = "PH",
-                    Name = "Photography",
-                    Description = "Nhiếp ảnh",
-                    RequiredScore = 20
-                },
-                new Department()
-                {
-                    Id = Guid.Parse("7l820b5a-3eac-4236-94ef-90b984aeb19h"),
-                    Code = "SC",
-                    Name = "Sculpture",
-                    Description = "Điêu khắc",
-                    RequiredScore = 22
-                },
-                new Department()
-                {
-                    Id = Guid.Parse("ao520b5a-4fac-4236-90ef-90b987aeb1ch"),
-                    Code = "FA",
-                    Name = "Fine Arts",
-                    Description = "Mỹ thuật",
-                    RequiredScore = 24
-                }
+                    Id = Guid.Parse("4c7c3b5f-2a54-42ee-8a7f-0961273cd329"),
+                    Code = "YH",
+                    Name = "Y học",
+                    Description = "Các ngành y dược",
+                    RequiredScore = 27
+                } 
                 );
         }
 
---
+++ ApplicationContext/Configurations/MajorConfiguration.cs
@@ -0,0 +1,133 @@
+﻿using ApplicationContext.Migrations;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationContext.Configurations
+{
+    public class MajorConfiguration : IEntityTypeConfiguration<Major>
+    {
+        public void Configure(EntityTypeBuilder<Major> builder)
+        {
+            builder.HasData(
+                new Major()
+                {
+                    Id = Guid.Parse("c731c3f5-a1d2-4b8f-bd7f-2d57fa0acb0f"),
+                    MajorName = "Kỹ thuật phần mềm",
+                    Code = "KTPM",
+                    Description = "Học về thiết kế phần mềm",
+                    DepartmentId = Guid.Parse("de305d54-75b4-431b-adb2-eb6b9e546013")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("63da08f2-e70f-488d-8d5a-eb24063697e5"),
+                    MajorName = "Khoa học máy tính",
+                    Code = "KHMT",
+                    Description = "Nghiên cứu lý thuyết và thuật toán máy tính",
+                    DepartmentId = Guid.Parse("de305d54-75b4-431b-adb2-eb6b9e546013")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("2b72f32c-5d96-4b5d-ae68-597bbeead505"),
+                    MajorName = "An toàn thông tin",
+                    Code = "ATTT",
+                    Description = "Nghiên cứu và bảo mật hệ thống thông tin",
+                    DepartmentId = Guid.Parse("de305d54-75b4-431b-adb2-eb6b9e546013")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("f5e68a91-1e8e-4ef7-9f0d-6da923dc5611"),
+                    MajorName = "Trí tuệ nhân tạo",
+                    Code = "TTNT",
+                    Description = "Nghiên cứu và ứng dụng trí tuệ nhân tạo",
+                    DepartmentId = Guid.Parse("de305d54-75b4-431b-adb2-eb6b9e546013")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("ce320b5a-4eac-4436-92ef-90b985aec10f"),
+                    MajorName = "Makerting",
+                    Code = "MK",
+                    Description = "Makerting",
+                    DepartmentId = Guid.Parse("b8034210-23fd-4af9-9d7f-f260a0cdef97")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("1b320b9a-3eac-4336-92ef-90b985aec101"),
+                    MajorName = "Quản trị kinh doanh",
+                    Code = "QTKD",
+                    Description = "Ngành học về quản lý và điều hành các hoạt động trong doanh nghiệp",
+                    DepartmentId = Guid.Parse("b8034210-23fd-4af9-9d7f-f260a0cdef97")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("2c430b7a-5eac-4536-93ef-80b985aec202"),
+                    MajorName = "Marketing",
+                    Code = "MKT",
+                    Description = "Ngành học về phân tích thị trường và xây dựng chiến lược marketing",
+                    DepartmentId = Guid.Parse("b8034210-23fd-4af9-9d7f-f260a0cdef97")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("3d550b8a-6eac-4636-92ef-70b985aec303"),
+                    MajorName = "Kinh doanh quốc tế",
+                    Code = "KDQT",
+                    Description = "Ngành học về kinh doanh và thương mại giữa các quốc gia",
+                    DepartmentId = Guid.Parse("b8034210-23fd-4af9-9d7f-f260a0cdef97")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("4e661b9a-7eac-4736-92ef-60b985aec404"),
+                    MajorName = "Kế toán",
+                    Code = "KT",
+                    Description = "Ngành học về công tác ghi chép, phân tích và kiểm tra các nghiệp vụ tài chính",
+                    DepartmentId = Guid.Parse("b8034210-23fd-4af9-9d7f-f260a0cdef97")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("1a111b2a-1eac-4132-92ef-90b985ae0101"),
+                    MajorName = "Y khoa",
+                    Code = "YK",
+                    Description = "Ngành học về chẩn đoán, điều trị bệnh và nâng cao sức khỏe",
+                    DepartmentId = Guid.Parse("4c7c3b5f-2a54-42ee-8a7f-0961273cd329")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("2b222b3a-2eac-4233-93ef-80b985ae0202"),
+                    MajorName = "Dược học",
+                    Code = "DH",
+                    Description = "Ngành học về nghiên cứu, phát triển và sản xuất thuốc",
+                    DepartmentId = Guid.Parse("4c7c3b5f-2a54-42ee-8a7f-0961273cd329")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("3c333b4a-3eac-4333-94ef-70b985ae0303"),
+                    MajorName = "Điều dưỡng",
+                    Code = "DD",
+                    Description = "Ngành học về chăm sóc người bệnh tại các cơ sở y tế",
+                    DepartmentId = Guid.Parse("4c7c3b5f-2a54-42ee-8a7f-0961273cd329")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("4d444b5a-4eac-4434-95ef-60b985ae0404"),
+                    MajorName = "Răng-Hàm-Mặt",
+                    Code = "RHM",
+                    Description = "Ngành học về phục hình, nha khoa và chỉnh nha",
+                    DepartmentId = Guid.Parse("4c7c3b5f-2a54-42ee-8a7f-0961273cd329")
+                },
+                new Major()
+                {
+                    Id = Guid.Parse("5e555b6a-5eac-4535-96ef-50b985ae0505"),
+                    MajorName = "Y học cổ truyền",
+                    Code = "YHCT",
+                    Description = "Ngành học về phương pháp chữa bệnh bằng y học cổ truyền",
+                    DepartmentId = Guid.Parse("4c7c3b5f-2a54-42ee-8a7f-0961273cd329")
+                }
+                );
+        }
+    }
+}
