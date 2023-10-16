ca599e39ce2a00450b8de27388e7a10b408da1ab
trinhtam <trinhtam2001@gmail.com>
2023-10-16 16:55:34 +0700

    University config

---
+++ ApplicationContext/Configurations/UniversityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationContext.Configurations
+{
+    public class UniversityConfiguration : IEntityTypeConfiguration<University>
+    {
+        public void Configure(EntityTypeBuilder<University> builder)
+        {
+           
+        }
+    }
+}
