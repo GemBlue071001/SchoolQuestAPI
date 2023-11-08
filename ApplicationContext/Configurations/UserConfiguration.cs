809c88c686ddb91bc6152ff407113f3c4d4f9281
trinhtam <trinhtam2001@gmail.com>
2023-11-08 15:05:20 +0700

    add admin

--- ApplicationContext/Configurations/UserConfiguration.cs
+++ ApplicationContext/Configurations/UserConfiguration.cs
@@ -20,20 +20,34 @@ namespace ApplicationContext.Configurations
         .HasDefaultValueSql("NOW()")
         .ValueGeneratedOnAdd();
 
-            var pass1 = CreatePasswordHash("student1");
+            var studentPass = CreatePasswordHash("student1");
+            var adminPass = CreatePasswordHash("admin");
 
             builder.HasData(new User
             {
                 Id = Guid.Parse("f0b84747-fbed-43e1-9cd1-ecf2e1bd3cc0"),
                 Email = "student@gmail.com",
-                PasswordHash = pass1.PasswordHash,
-                PasswordSalt = pass1.PasswordSalt,
+                PasswordHash = studentPass.PasswordHash,
+                PasswordSalt = studentPass.PasswordSalt,
                 FirstName = "Nguyen",
                 LastName = "Vu",
                 Role = Domain.Enums.UserRole.Student,
                 UserName = "student1",
                 PhoneNumber = "1234567890",
-            });
+            },
+            new User
+            {
+                Id = Guid.Parse("a6be20e2-5927-4cce-bed4-258f919f82e6"),
+                Email = "admin@gmail.com",
+                PasswordHash = adminPass.PasswordHash,
+                PasswordSalt = adminPass.PasswordSalt,
+                FirstName = "admin",
+                LastName = "admin",
+                Role = Domain.Enums.UserRole.Admin,
+                UserName = "admin",
+                PhoneNumber = "1234567890",
+            }
+            );
         }
 
 
