0e1f830102eb82176eb3f2b0f932d565f8f40656
Dai Tran <80236128+KawaiiScorpy@users.noreply.github.com>
2023-12-04 12:47:56 +0700

    Update UniversityConfiguration.cs

--- ApplicationContext/Configurations/UniversityConfiguration.cs
+++ ApplicationContext/Configurations/UniversityConfiguration.cs
@@ -53,42 +53,42 @@ namespace ApplicationContext.Configurations
                 {
                     Id = Guid.Parse("83e4e060-f70a-4fb3-b828-e8f1443d3f46"),
                     Name = "Trường Đại học Bách Khoa TP.Hồ Chí Minh",
-                    Code = "QSB",
+                    Code = "HCMUT",
                     Description = "Trường đại học đầu ngành về lĩnh vực kỹ thuật ở miền Nam Việt Nam"
                 },
                 new University()
                 {
                     Id = Guid.Parse("b9647f7b-e9a0-4636-80f9-45364c8b04a9"),
                     Name = "Trường Đại học Công nghệ Thông tin TP.HCM",
-                    Code = "QSC",
+                    Code = "UIT",
                     Description = "Trường đại học công lập đào tạo về công nghệ thông tin và truyền thông"
                 },
                 new University()
                 {
                     Id = Guid.Parse("54812146-c3a3-41e1-9b46-c24a2a0a2aa1"),
                     Name = "Trường Đại học Khoa học Tự nhiên TPHCM",
-                    Code = "QST",
+                    Code = "HCMUS",
                     Description = "Trường đại học đầu ngành về đào tạo, nghiên cứu khoa học cơ bản, khoa học công nghệ và ứng dụng ở miền Nam Việt Nam."
                 },
                 new University()
                 {
                     Id = Guid.Parse("b8ec3722-2649-4738-8316-38ffd56ed04d"),
                     Name = "Trường Đại học Khoa học Xã hội và Nhân văn TP.HCM",
-                    Code = "QSX",
+                    Code = "HCMUSSH",
                     Description = "Trường là trung tâm nghiên cứu, đào tạo trong lĩnh vực khoa học xã hội và nhân văn lớn nhất miền Nam."
                 },
                 new University()
                 {
                     Id = Guid.Parse("314a233c-a95f-4026-992a-75a708d07838"),
-                    Name = "Đại học Kiến trúc Thành phố Hồ Chí Minh",
-                    Code = "QSK",
+                    Name = "Trường Đại học Kinh tế – Luật TP.HCM",
+                    Code = "UEL",
                     Description = "Trường đại học đào tạo và nghiên cứu khối ngành kinh tế, kinh doanh quản lý và luật hàng đầu Việt Nam"
                 },
                 new University()
                 {
                     Id = Guid.Parse("2a5d2d04-9d41-4279-9337-5b0039ee1b23"),
                     Name = "Trường Đại học Quốc tế",
-                    Code = "QSQ",
+                    Code = "HCMIU",
                     Description = "Trường đại học công lập đa ngành đầu tiên tại Việt Nam giảng dạy hoàn toàn tiếng Anh"
                 },
                 new University()
@@ -160,6 +160,69 @@ namespace ApplicationContext.Configurations
                           Name = "Trường Đại học Văn hóa TP.HCM",
                           Code = "VHS",
                           Description = "Trường đại học đào tạo và nghiên cứu khoa học các lĩnh vực: văn hóa, nghệ thuật, thông tin và du lịch."
+                      },
+                      new University()
+                      {
+                          Id = Guid.Parse("cdbd71b3-7fe4-4917-9eed-616b4c2f2a97"),
+                          Name = "Trường Đại học Sư phạm - Kỹ thuật TP.HCM",
+                          Code = "HCMUE",
+                          Description = "Trường đại học đa ngành tại Việt Nam, với thế mạnh về đào tạo kỹ thuật"
+                      },
+                      new University()
+                      {
+                          Id = Guid.Parse("253fca55-c797-4f1d-8c83-a66d81342921"),
+                          Name = "Trường Đại học Công nghiệp TP.HCM",
+                          Code = "IUH",
+                          Description = "Trường đại học định hướng ứng dụng và thực hành, chuyên đào tạo nhóm ngành kinh tế công nghiệp và kỹ thuật công nghiệp"
+                      },
+                      new University()
+                      {
+                          Id = Guid.Parse("ddb202ca-e224-48d6-a463-6b32ed962693"),
+                          Name = "Trường Đại học Giao thông Vận tải TP.HCM",
+                          Code = "GSA",
+                          Description = "Trường đại học đa ngành thuộc lĩnh vực giao thông vận tải lớn nhất phía Nam Việt Nam."
+                      },
+                      new University()
+                      {
+                          Id = Guid.Parse("778c4103-05d3-4dab-9019-7196f6772dc8"),
+                          Name = "Trường Đại học Sân khấu – Điện ảnh TP.HCM",
+                          Code = "SKDAHCM",
+                          Description = "Trường đại học chuyên đào tạo nhóm ngành sân khấu, điện ảnh và nghệ thuật tại Thành phố Hồ Chí Minh."
+                      },
+                      new University()
+                      {
+                          Id = Guid.Parse("abf76dc2-7086-4836-badb-a520e720caaa"),
+                          Name = "Trường Đại học Tài nguyên – Môi trường TPHCM",
+                          Code = "HCMUNRE",
+                          Description = "Trường đại học đào tạo các chuyên ngành kinh tế, kỹ thuật, môi trường, khí hậu, biển - hải đảo, trắc địa - bản đồ, đất đai, địa chất, khí tượng, thủy văn,..."
+                      },
+                      new University()
+                      {
+                          Id = Guid.Parse("166d0a24-7e52-45af-a795-d8e082d3749a"),
+                          Name = "Trường Đại học Tài chính – Marketing",
+                          Code = "UFM",
+                          Description = "Trường đại học công lập chuyên ngành về nhóm ngành tài chính và quản lý tại miền Nam Việt Nam"
+                      },
+                      new University()
+                      {
+                          Id = Guid.Parse("b83746d5-641b-40ef-9804-a8fb7ae9fb4f"),
+                          Name = "Trường Đại học Trần Đại Nghĩa",
+                          Code = "TDNU",
+                          Description = "Trường đại học kỹ thuật được thành lập ngày 12 tháng 05 năm 1975 trên cơ sở nâng cấp Trường Sĩ quan Kỹ thuật quân sự."
+                      },
+                      new University()
+                      {
+                          Id = Guid.Parse("98f712a7-3847-46f7-90cc-2e3cb2348123"),
+                          Name = "Trường Đại học Lao động – Xã hội cơ sở 2",
+                          Code = "ULSA2",
+                          Description = "Trường đại học đào tạo các khối ngành kinh tế, xã hội; nghiên cứu và ứng dụng khoa học phục vụ giáo dục, đào tạo và phát triển kinh tế - xã hội"
+                      },
+                      new University()
+                      {
+                          Id = Guid.Parse("7d8960c2-ee10-4813-9522-106512a9b802"),
+                          Name = "Trường Đại học Sư phạm Thể dục Thể thao",
+                          Code = "UPES",
+                          Description = "Trường đại học đào tạo về giáo dục thể chất và đào tạo chuẩn hóa giáo viên thể dục theo định chuẩn của Bộ Giáo dục và Đào tạo Việt Nam."
                       }
                 );
         }
